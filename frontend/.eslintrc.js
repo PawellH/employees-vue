40bde557f022c68450b6a0a06ceca5855b2f3234
PawellH <pavelgoncharov97@gmail.com>
2021-02-14 13:29:52 +0300

    Add rules ESLint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,14 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    "plugin:vue/essential",
+    "@vue/airbnb",
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
+    "import/no-unresolved": "off",
+    "no-control-regex": "off",
+    "arrow-body-style": ["error", "always"],
+    "no-param-reassign": "off",
+    "no-mixed-operators": "off",
+    "max-len": "off",
   },
 };
