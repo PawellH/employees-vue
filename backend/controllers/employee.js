2d8cfdecbb2514caced20114a689f52732d5a52c
ilyavysotski <ilubvys@gmail.com>
2021-02-24 19:33:54 +0300

    feat: add include and fields filters for /employee endpoint

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -2,20 +2,70 @@ const createError = require("http-errors");
 const asyncHandler = require("express-async-handler");
 const connection = require("../boot/dbConnection");
 
+function parseQueryFieldsString(queryFieldsString) {
+  const fieldsObject = {
+    id: "employee.id",
+    lastname: "employee.lastname",
+    first_name: "employee.first_name",
+    middle_name: "employee.middle_name",
+    phone: "employee.phone",
+    email: "employee.email",
+    date_birth: "employee.date_birth",
+    date_receipt: "employee.date_receipt",
+    date_dismissal: "employee.date_dismissal",
+    address: "employee.address",
+    place_birth: "employee.place_birth",
+  }
+  const queryFields = queryFieldsString.split(",").map(queryField => {
+    return queryField.trim();
+  });
+  const parsed = queryFields.reduce((acc, queryField) => {
+    const field = fieldsObject[queryField];
+    if (field) {
+      acc.push(field);
+    }
+    return acc;
+  }, []);
+  return parsed;
+}
+
+function parseQueryIncludeString(includeQueryString) {
+  const fieldsObject = {
+    position: "position.name"
+  };
+  const joinsObject = {
+    position: "inner join position on employee.id_position = position.id"
+  };
+  const fieldsToInclude = includeQueryString.split(",").map(fieldToInclude => {
+    return fieldToInclude.trim();
+  });
+  const parsed = fieldsToInclude.reduce((acc, fieldToInclude) => {
+    const field = fieldsObject[fieldToInclude];
+    const join = joinsObject[fieldToInclude];
+    if (field) {
+      acc.fields.push(field);
+    }
+    if (join) {
+      acc.joins.push(join);
+    }
+    return acc;
+  }, { fields: [], joins: []});
+  return parsed;
+}
+
 exports.get = asyncHandler(async function(req, res) {
-  const { id, fields: fieldsString } = req.query;
+  const { id, fields: queryFieldsString = "", include: includeQueryString = "" } = req.query;
   let rows = null;
   if (id) {
     const ids = id.split(",");
-    const fields = fieldsString.split(",");
-    debugger;
-    connection.
-    rows = (await connection.execute(`
-    SELECT middle_name FROM employee as e
-    inner join position
-    on e.id_position = position.id
-    WHERE e.id in (:ids);
-    `, { ids, fields }))[0];
+    const parsedQueryFieldsString = parseQueryFieldsString(queryFieldsString);
+    const parsedIncludeQueryString = parseQueryIncludeString(includeQueryString);
+    const fieldsToSelect = [...parsedQueryFieldsString, ...parsedIncludeQueryString.fields]
+    rows = (await connection.query(`
+    SELECT ${fieldsToSelect.length > 0 ? fieldsToSelect : "*"} FROM employee
+    ${parsedIncludeQueryString.joins.join("\n")}
+    WHERE employee.id in (:ids);
+    `, { ids }))[0];
   } else {
     rows = (await connection.query("SELECT * FROM employee"))[0];
   }
--- frontend/src/helpers/httpClient.js
+++ frontend/src/helpers/httpClient.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log(process.env);
 export default axios.create({
   withCredentials: true,
   baseURL: process.env.VUE_APP_API_URL,
